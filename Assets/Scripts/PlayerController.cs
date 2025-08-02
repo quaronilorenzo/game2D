f791fe9399e91aa2c961444bd174a2bfb7cd092e
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2025-08-02 09:07:38 +0200

    cambiato input movement, value invece che button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,29 +4,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 6f;
-
+    public InputAction LeftAction;
+    public InputAction MoveAction;
+    void Start()
+    {
+        MoveAction.Enable();
+    }
     void Update()
     {
-        if (Keyboard.current == null)
-            return;
-
-        float horizontal = 0f;
-        float vertical = 0f;
-
-        if (Keyboard.current.aKey.isPressed)
-            horizontal = -1f;
-        else if (Keyboard.current.dKey.isPressed)
-            horizontal = 1f;
-
-        if (Keyboard.current.wKey.isPressed)
-            vertical = 1f;
-        else if (Keyboard.current.sKey.isPressed)
-            vertical = -1f;
-
-        Vector2 movement = new Vector2(horizontal, vertical);
-        if (movement.sqrMagnitude > 1f)
-            movement.Normalize();
-
-        transform.Translate(movement * speed * Time.deltaTime);
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        transform.position = position;
     }
 }
