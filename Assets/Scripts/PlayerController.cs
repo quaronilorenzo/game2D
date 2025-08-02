e5032d17bd77933d5dbcc913450524d06c12b9b5
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2025-08-02 09:36:31 +0200

    reso indipendente il framerate, aggiunto time.deltatime

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6f;
+    public float speed = 3f;
     public InputAction LeftAction;
     public InputAction MoveAction;
     void Start()
     {
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 120;
         MoveAction.Enable();
     }
     void Update()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
     }
 }
