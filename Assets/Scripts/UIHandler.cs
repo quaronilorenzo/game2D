f3ab552f444ca00ba826422ae56605630c07a689
Quaro <135996757+quaronilorenzo@users.noreply.github.com>
2025-08-08 18:12:20 +0200

    aggiunta health bar

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+public class UIHealthbar : MonoBehaviour
+{
+    void Start()
+    {
+        UIDocument uiDocument = GetComponent<UIDocument>();
+    }
+    void Update()
+    {
+        
+    }
+}
